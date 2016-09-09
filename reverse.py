f14bb3ecf77b1c798677d8577d25011933d276fd
Durvesh Pilankar <durveshpilankar92@gmail.com>
2016-09-09 09:59:27 -0700

    Update reverse.py

--- reverse.py
+++ reverse.py
@@ -1,20 +1,20 @@
 """ Reverse a Number"""
 import sys
 
-mynum=int(raw_input("Please Enter the number: "))
+mynum = int(raw_input("Please Enter the number: "))
 print "Your Number is: ",mynum
 
 #if length of integer == 1, reverse is same
-if(len(str(mynum))==1):
+if(len(str(mynum)) == 1):
     print "Reverse of the number is: ",mynum
     sys.exit()
 
-temp=mynum    
-rev=0
+temp = mynum    
+rev = 0
 
 #looping till the temp is not equal to zero. Also, creating temp so that original value not lost
-while(temp!=0):
-    rev=rev*10
-    rev=rev+temp%10
-    temp=temp/10
+while(temp != 0):
+    rev = rev*10
+    rev = rev + temp % 10
+    temp = temp / 10
 print  "Reverse of the number is: ",rev    
