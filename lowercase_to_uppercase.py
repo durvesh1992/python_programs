efdf044e1fe9c71279cd4d7c0e820809f66aa219
Durvesh Pilankar <durveshpilankar92@gmail.com>
2016-09-16 11:10:16 -0700

    Update lowercase_to_uppercase.py

--- lowercase_to_uppercase.py
+++ lowercase_to_uppercase.py
@@ -8,11 +8,25 @@ O/p = SAN JOSE STATE UNIVERSITY
 
 import sys
 
+
 def lower_to_upper(mystr):
     return mystr.upper()
 
+def lower_to_upper_ascii(mystr):
+    upperCaseWord = ""
+    for i in mystr:
+        #print "i = " + i + "and Asci value = " +str(ord(i))
+
+        if i == " ":
+            upperCaseWord += " "
+
+        j = ord(i) - 32
+        upperCaseWord += chr(j)
+
+    return upperCaseWord
+
+
 mystr = "san jose state university"
+
 print lower_to_upper(mystr)
-
-#print mystr.upper()
-
+print lower_to_upper_ascii(mystr)
