83b88cfd901c01ee8fdfa3f7b58e4f0f0785b144
Durvesh Pilankar <durveshpilankar92@gmail.com>
2016-09-16 10:54:03 -0700

    Create lowercase_to_uppercase.py

---
+++ lowercase_to_uppercase.py
@@ -0,0 +1,18 @@
+"""
+Lower case to upper case
+
+I/p = san jose state university
+
+O/p = SAN JOSE STATE UNIVERSITY
+"""
+
+import sys
+
+def lower_to_upper(mystr):
+    return mystr.upper()
+
+mystr = "san jose state university"
+print lower_to_upper(mystr)
+
+#print mystr.upper()
+
