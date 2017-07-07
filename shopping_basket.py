7f07463f7aa3c412014b5cf4abb4fc4e7a5fa92b
Durvesh Pilankar <durveshpilankar92@gmail.com>
2017-07-07 16:25:42 -0700

    Shopping basket

---
+++ shopping_basket.py
@@ -0,0 +1,23 @@
+#Shopping Basket
+
+shopping_basket = {}
+
+print "Shopping Basket Options: \n" \
+      "1: Add Items \n" \
+      "2: Remove Items \n" \
+      "3: View Basket \n" \
+      "0: Exit Program \n"
+
+option = int(input("Enter an option: "))
+while option != 0:
+    if option == 1:
+        pass
+    elif option == 2:
+        pass
+    elif option == 3:
+        pass
+    elif option != 0:
+        print "Please enter the correct option "
+    pass
+
+print "Shoppnig Basket is closed"
